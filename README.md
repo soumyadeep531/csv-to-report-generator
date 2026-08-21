fb3434785ddaeb5895ec072c4d2ca63b44a08277
Soumyadeep Giri <soumyadeepgiri5@gmail.com>
2026-08-21 14:29:40 +0530

    issue-2: i have updated readme file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # csv-to-report-generator
 csv to deatiled summary
+this is for the project used fastapi for cv to data analysis pdf generation 
+
--- app/main.py
+++ app/main.py
@@ -1,28 +1,109 @@
 from fastapi import FastAPI
-app=FastAPI()
-students=[
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+    course: str
+
+
+students = [
     {
-        "id":1,
-        "name":"Rahul",
-        "age":20,
-        "course":"python"
+        "id": 1,
+        "name": "Rahul",
+        "age": 20,
+        "course": "Python"
     },
     {
-            "id":2,
-            "name":"priya",
-            "age":22,
-            "course":"FastAPI"
-        }
+        "id": 2,
+        "name": "Priya",
+        "age": 22,
+        "course": "FastAPI"
+    }
 ]
+
 
 @app.get("/")
 def home():
-    return{
-        "message":"Welcome to student management api"
+    return {
+        "message": "Welcome to Student Management API"
     }
+
 
 @app.get("/students")
 def get_students():
     return {
-        "students":students
+        "students": students
+    }
+
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+
+    for student in students:
+        if student["id"] == student_id:
+            return student
+
+    return {
+        "message": "Student not found"
+    }
+
+
+@app.post("/students")
+def add_student(student: Student):
+
+    new_student = {
+        "id": len(students) + 1,
+        "name": student.name,
+        "age": student.age,
+        "course": student.course
+    }
+
+    students.append(new_student)
+
+    return {
+        "message": "Student added successfully",
+        "student": new_student
+    }
+
+
+@app.put("/students/{student_id}")
+def update_student(student_id: int, updated_student: Student):
+
+    for student in students:
+
+        if student["id"] == student_id:
+
+            student["name"] = updated_student.name
+            student["age"] = updated_student.age
+            student["course"] = updated_student.course
+
+            return {
+                "message": "Student updated successfully",
+                "student": student
+            }
+
+    return {
+        "message": "Student not found"
+    }
+
+
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int):
+
+    for student in students:
+
+        if student["id"] == student_id:
+
+            students.remove(student)
+
+            return {
+                "message": "Student deleted successfully"
+            }
+
+    return {
+        "message": "Student not found"
     }
